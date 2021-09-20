92a2dff2665e7803fa49b0cf4ab78862d170e47d
fangyinglim <limfy97@gmail.com>
2021-09-20 12:00:12 +0800

    added css styling on hover for nav bar

---
+++ main.js
@@ -0,0 +1,10 @@
+// const listItems = document.querySelectorAll('li');
+
+
+// listItems.forEach( list => {
+//     list.addEventListener('mouseover', hoverEffect)
+//     });
+
+// function hoverEffect() {
+//     console.log(this);
+// }
