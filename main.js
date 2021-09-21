55f4314947920b7202845faf9fb9e06f523a88c5
fangyinglim <limfy97@gmail.com>
2021-09-21 13:06:21 +0800

    added dropdown bar for get started nav link

--- main.js
+++ main.js
@@ -8,3 +8,18 @@
 // function hoverEffect() {
 //     console.log(this);
 // }
+
+const getStarted = document.querySelector('#start');
+const newDiv = document.createElement('div');
+newDiv.className = 'start-menu';
+getStarted.addEventListener('mouseover', addDropDown);
+getStarted.addEventListener('mouseleave', removeDropDown);
+
+function addDropDown() {
+    console.log('test');
+    getStarted.appendChild(newDiv);
+}
+
+function removeDropDown() {
+    getStarted.removeChild(newDiv);
+}
