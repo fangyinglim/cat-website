e080d6d5ad485fcfb3b3a3e6c620da7efd11c177
fangyinglim <limfy97@gmail.com>
2021-09-23 13:45:58 +0800

    added intersection observer, mediaquery for homepage at width 900px

--- main.js
+++ main.js
@@ -16,10 +16,36 @@ getStarted.addEventListener('mouseover', addDropDown);
 getStarted.addEventListener('mouseleave', removeDropDown);
 
 function addDropDown() {
-    console.log('test');
+    // console.log('test');
     getStarted.appendChild(newDiv);
 }
 
 function removeDropDown() {
     getStarted.removeChild(newDiv);
 }
+
+
+
+let options = {
+    root: null,
+    rootMargin: '0px 0px -200px 0px',
+    threshold: 0.5
+};
+let callback = (entries,observer) => {
+    entries.forEach(entry => {
+        // console.log(entry.isIntersecting);
+        // console.log(entry.target)
+        if (!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.classList.add('appear');
+            observer.unobserve(entry.target)
+        }
+    })};
+
+let observer = new IntersectionObserver(callback, options);
+let sectionTwoTarget = document.querySelectorAll('.image')
+
+sectionTwoTarget.forEach ( image => {
+    observer.observe(image);
+})
