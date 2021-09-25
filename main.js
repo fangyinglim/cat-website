8c939a02c125700fe8767ab97d1ea9c8f00a4bd5
fangyinglim <limfy97@gmail.com>
2021-09-25 21:27:40 +0800

    added images, srcset for responsiveness

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function removeDropDown() {
 
 let options = {
     root: null,
-    rootMargin: '0px 0px -200px 0px',
+    rootMargin: '0px',
     threshold: 0.5
 };
 let callback = (entries,observer) => {
